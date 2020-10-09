e65971531e5c4c04b5ee146f83a458dfea26d1e6
viniciussec <viniciussec@hotmail.com>
2020-10-09 17:32:37 -0300

    adjusting exceptions

--- src/entities/MyTwitter.java
+++ src/entities/MyTwitter.java
@@ -1,76 +1,136 @@
 package entities;
+
 import java.util.ArrayList;
+
+import exceptions.MFPException;
+import exceptions.PDException;
+import exceptions.PEException;
+import exceptions.PIException;
+import exceptions.SIException;
+import exceptions.UJCException;
 
 public class MyTwitter implements ITwitter {
 	private IRepositorioUsuario repositorio;
-	
+
 	public MyTwitter(IRepositorioUsuario repositorio) {
 		this.repositorio = repositorio;
 	}
-	
+
 	@Override
-	public void criarPerfil(Perfil usuario) {
-		repositorio.cadastrar(usuario);
+	public void criarPerfil(Perfil usuario) throws PEException {
+		try {
+			repositorio.cadastrar(usuario);
+		} catch (UJCException e) {
+			throw new PEException(usuario.getUsuario());
+		}
 	}
 
 	@Override
-	public void cancelarPerfil(String usuario) {
+	public void cancelarPerfil(String usuario) throws PIException, PDException {
+		if (repositorio.buscar(usuario) == null) {
+			throw new PIException(usuario);
+		}
+		if (repositorio.buscar(usuario).isAtivo() == false) {
+			throw new PDException(usuario);
+		}
 		repositorio.buscar(usuario).setAtivo(false);
 	}
 
 	@Override
-	public void tweetar(String usuario, String mensagem) {
-		if(mensagem.length()<=140) {
-			Tweet tweet = new Tweet();
-			tweet.setMensagem(mensagem);
-			tweet.setUsuario(usuario);
-			
-			repositorio.buscar(usuario).addTweet(tweet);
+	public void tweetar(String usuario, String mensagem) throws MFPException, PIException {
+		if (repositorio.buscar(usuario) == null) {
+			throw new PIException(usuario);
 		}
-		else {
-			System.out.println("Mensagem muito grande!");
+		if (mensagem.length() > 140) {
+			throw new MFPException(mensagem);
 		}
+		Tweet tweet = new Tweet();
+		tweet.setMensagem(mensagem);
+		tweet.setUsuario(usuario);
+		repositorio.buscar(usuario).addTweet(tweet);
 	}
 
 	@Override
-	public ArrayList<Tweet> timeline(String usuario) {
+	public ArrayList<Tweet> timeline(String usuario) throws PIException, PDException {
+		if (repositorio.buscar(usuario) == null) {
+			throw new PIException(usuario);
+		}
+		if (repositorio.buscar(usuario).isAtivo() == false) {
+			throw new PDException(usuario);
+		}
 		return repositorio.buscar(usuario).getTimeline();
 	}
 
 	@Override
-	public ArrayList<Tweet> tweets(String usuario) {
+	public ArrayList<Tweet> tweets(String usuario) throws PIException, PDException {
+		if (repositorio.buscar(usuario) == null) {
+			throw new PIException(usuario);
+		}
+		if (repositorio.buscar(usuario).isAtivo() == false) {
+			throw new PDException(usuario);
+		}
+
 		ArrayList<Tweet> tweets = new ArrayList<>();
-		
 		ArrayList<Tweet> timeline = repositorio.buscar(usuario).getTimeline();
-		
-		for(Tweet t : timeline) {
-			if(t.getUsuario() == usuario) {
+		for (Tweet t : timeline) {
+			if (t.getUsuario() == usuario) {
 				tweets.add(t);
 			}
 		}
-		
 		return tweets;
 	}
 
 	@Override
-	public void seguir(String seguidor, String seguido) {
+	public void seguir(String seguidor, String seguido) throws PIException, PDException, SIException {
+		if (repositorio.buscar(seguidor) == null) {
+			throw new PIException(seguidor);
+		}
+		if (repositorio.buscar(seguidor).isAtivo() == false) {
+			throw new PDException(seguidor);
+		}
+		if (repositorio.buscar(seguido) == null) {
+			throw new PIException(seguido);
+		}
+		if (repositorio.buscar(seguido).isAtivo() == false) {
+			throw new PDException(seguido);
+		}
+		if (seguidor == seguido) {
+			throw new SIException(seguidor, seguido);
+		}
 		repositorio.buscar(seguidor).addSeguido(repositorio.buscar(seguido));
 		repositorio.buscar(seguido).addSeguidor(repositorio.buscar(seguidor));
 	}
 
 	@Override
-	public int numeroSeguidores(String usuario) {
+	public int numeroSeguidores(String usuario) throws PIException, PDException {
+		if (repositorio.buscar(usuario) == null) {
+			throw new PIException(usuario);
+		}
+		if (repositorio.buscar(usuario).isAtivo() == false) {
+			throw new PDException(usuario);
+		}
 		return repositorio.buscar(usuario).getSeguidores().size();
 	}
 
 	@Override
-	public ArrayList<Perfil> seguidores(String usuario) {
+	public ArrayList<Perfil> seguidores(String usuario) throws PIException, PDException {
+		if (repositorio.buscar(usuario) == null) {
+			throw new PIException(usuario);
+		}
+		if (repositorio.buscar(usuario).isAtivo() == false) {
+			throw new PDException(usuario);
+		}
 		return repositorio.buscar(usuario).getSeguidores();
 	}
 
 	@Override
-	public ArrayList<Perfil> seguidos(String usuario) {
+	public ArrayList<Perfil> seguidos(String usuario) throws PIException, PDException {
+		if (repositorio.buscar(usuario) == null) {
+			throw new PIException(usuario);
+		}
+		if (repositorio.buscar(usuario).isAtivo() == false) {
+			throw new PDException(usuario);
+		}
 		return repositorio.buscar(usuario).getSeguidos();
 	}
-
 }
