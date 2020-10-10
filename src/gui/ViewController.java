fe9405b6feacf5dfd03f1e607bd985a92bc41aa4
viniciussec <viniciussec@hotmail.com>
2020-10-10 11:23:56 -0300

    adding myTwitter views

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -19,6 +19,12 @@ import javafx.scene.layout.VBox;
 public class ViewController implements Initializable {
 
 	@FXML
+	private MenuItem menuItemBuscar;
+
+	@FXML
+	private MenuItem menuItemAtualizar;
+
+	@FXML
 	private MenuItem menuItemPessoaFisica;
 
 	@FXML
@@ -28,13 +34,40 @@ public class ViewController implements Initializable {
 	private MenuItem menuItemAbout;
 
 	@FXML
+	private MenuItem menuItemCriarPerfil;
+
+	@FXML
+	private MenuItem menuItemCancelarPerfil;
+
+	@FXML
+	private MenuItem menuItemTweetar;
+
+	@FXML
+	private MenuItem menuItemTimeline;
+
+	@FXML
+	private MenuItem menuItemTweets;
+	
+	@FXML
+	private MenuItem menuItemSeguir;
+
+	@FXML
+	private MenuItem menuItemNumeroSeguidores;
+
+	@FXML
+	private MenuItem menuItemSeguidores;
+
+	@FXML
+	private MenuItem menuItemSeguidos;
+
+	@FXML
 	public void onMenuItemPessoaFisicaAction() {
-		System.out.println("onMenuItemPessoaFisicaAction");
+		loadView("/gui/PessoaFisicaCadastro.fxml");
 	}
 
 	@FXML
 	public void onMenuItemPessoaJuridicaAction() {
-		System.out.println("onMenuItemPessoaJuridicaAction");
+		loadView("/gui/PessoaJuridicaCadastro.fxml");
 	}
 
 	@FXML
@@ -42,9 +75,63 @@ public class ViewController implements Initializable {
 		loadView("/gui/About.fxml");
 	}
 
+	@FXML
+	public void onMenuItemBuscarAction() {
+		loadView("/gui/Buscar.fxml");
+	}
+
+	@FXML
+	public void onMenuItemAtualizarAction() {
+		loadView("/gui/Atualizar.fxml");
+	}
+
+	@FXML
+	public void onMenuItemCriarPerfilAction() {
+		loadView("/gui/CriarPerfil.fxml");
+	}
+
+	@FXML
+	public void onMenuItemCancelarPerfilAction() {
+		loadView("/gui/CancelarPerfil.fxml");
+	}
+
+	@FXML
+	public void onMenuItemTweetarAction() {
+		loadView("/gui/Tweetar.fxml");
+	}
+
+	@FXML
+	public void onMenuItemTimelineAction() {
+		loadView("/gui/Timeline.fxml");
+	}
+
+	@FXML
+	public void onMenuItemTweetsAction() {
+		loadView("/gui/Tweets.fxml");
+	}
+
+	@FXML
+	public void onMenuItemSeguirAction() {
+		loadView("/gui/Seguir.fxml");
+	}
+
+	@FXML
+	public void onMenuItemNumeroSeguidoresAction() {
+		loadView("/gui/NumeroSeguidores.fxml");
+	}
+
+	@FXML
+	public void onMenuItemSeguidoresAction() {
+		loadView("/gui/Seguidores.fxml");
+	}
+
+	@FXML
+	public void onMenuItemSeguidosAction() {
+		loadView("/gui/Seguidos.fxml");
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -52,16 +139,15 @@ public class ViewController implements Initializable {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
-			
+
 			Scene mainScene = Main.getMainScene();
 			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
+
 			Node mainMenu = mainVBox.getChildren().get(0);
 			mainVBox.getChildren().clear();
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			
+
 		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Erro carregando página", e.getMessage(), AlertType.ERROR);
 		}
